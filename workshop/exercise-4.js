55cdc0103afb89fa2722facdb20e7f3d3f241f80
Antonio Placido <tonyplacido@live.com>
2020-06-17 01:10:23 -0400

    Question 4

--- workshop/exercise-4.js
+++ workshop/exercise-4.js
@@ -6,3 +6,17 @@ function calculateAverage(a, b, c, d, e) {}
 
 // make certain to console the returned value
 console.log(calculateAverage(76, 60, 83, 100, 78));
+
+
+/* gonna use length of array to be the divider, then use the sum length.array times to get answer */
+
+
+
+let grades = [76, 60, 83, 100, 78];
+
+let total = 0;
+for(let i=0;i< grades.length; i++ )
+        {total += grades[i];}
+
+const avg = Math.trunc(total / grades.length);
+console.log(avg);
