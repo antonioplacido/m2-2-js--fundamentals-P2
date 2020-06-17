e9311153227d4d0055b0aae412c6d0037199fe9f
Antonio Placido <tonyplacido@live.com>
2020-06-17 03:53:33 -0400

    Question 7

--- workshop/exercise-7.js
+++ workshop/exercise-7.js
@@ -1,4 +1,4 @@
-// You are not allowed to for or while loops.
+// You are not allowed to for or while loops.  
 // Instead focus on the array methods (filter, map, forEach, etc)
 
 // You will also need to verify that your functions work as expected. Be sure to test them. :)
@@ -6,10 +6,13 @@
 // Q7
 // Returns a new list with all the elements of lst that are length greater than 5
 function keepLong(lst) {
-  // lst is an array of strings
+  
+const bigBoi = lst.filter(function(word){
+  return word.length >5;
+})
+
+return bigBoi
 }
 // -------------------------------------------------------------------------
-console.log(
-  'Q5 keepLong()',
-  keepLong(['Cyborg', 'Robin', 'Batman', 'Superman', 'Aquaman', 'Flash'])
-);
+console.log('Q5 keepLong()',keepLong(['Cyborg', 'Robin', 'Batman', 'Superman', 'Aquaman', 'Flash']));
+
