918cbe0fba9775f03faad1703e0c2a5768eed091
Antonio Placido <aplacido1992@gmail.com>
2020-06-28 00:18:51 -0400

    Question 12

--- workshop/exercise-12.js
+++ workshop/exercise-12.js
@@ -14,21 +14,36 @@
 
 // Try with other values as well
 
-
 function calculateAverage(grades) {
-    const total = 0;    
-    const avg = grades(function (number)
-    
-    //*this part i could never get  *//
-    
-    grades.forEach(function (grade) {
-        sum += grade;
-    /*I              */
-    
-    return Math.trunc(total / grades.length);
-    }
-    
-    console.log(calculateAverage([76, 60, 83, 100, 78]));
-    
-    
-    
+  let gradesum = 0;
+
+  //*this part i could never get  *//
+  grades.forEach(function (grade) {
+    gradesum += grade;
+  });
+
+  let avgGrade = Math.trunc(gradesum / grades.length);
+  switch (true) {
+    case avgGrade < 60:
+      return `F`;
+      break;
+    case avgGrade < 70:
+      return `D`;
+      break;
+    case avgGrade < 80:
+      return `C`;
+      break;
+    case avgGrade < 90:
+      return `B`;
+      break;
+    case avgGrade <= 100:
+      return `A`;
+      break;
+  }
+}
+
+console.log(calculateAverage([76, 60, 83, 100, 78]));
+
+console.log(calculateAverage([48, 95, 65, 48, 59, 78, 72, 65]));
+
+/////***Need to do this workshop question */
